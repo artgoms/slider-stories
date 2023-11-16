f547c8ee7b0995e007d08d2cd1dcc8010b1711f5
Artur Gomes <arturg91@outlook.com>
2023-11-15 23:18:19 -0300

    mod js archive

--- slide-stories.js
+++ slide-stories.js
@@ -1,9 +1,17 @@
 class SlideStories {
-    constructor(id) {
-        this.slide = document.querySelector(`[data-slide="${id}"]`)
-        this.active = 0
-        console.log(this.slide )
-    }
+  constructor(id) {
+    this.slide = document.querySelector(`[data-slide="${id}"]`)
+    this.active = 0
+    console.log(this.slide)
+    this.activeSlide(2)
+  }
+
+  activeSlide(index) {
+    this.active = index
+    this.items = this.slide.querySelectorAll('.slide-items > *')
+    this.items[index].classList.add('active')
+    console.log()
+  }
 }
 
 new SlideStories('slide')
